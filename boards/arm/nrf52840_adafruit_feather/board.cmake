a7d251d256572ab55a0134d510e4d7e095047118
Tobias Svehagen <tobias.svehagen@gmail.com>
2019-09-08 17:14:45 +0200

    boards: arm: Add Adafruit Feather nRF52840 board
    TODO:
    - Fix doc/index.rst
    - Test
    - SPI Flash?

---
+++ boards/arm/nrf52840_adafruit_feather/board.cmake
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(nrfjprog "--nrf-family=NRF52")
+board_runner_args(jlink "--device=nrf52" "--speed=4000")
+board_runner_args(pyocd "--target=nrf52" "--frequency=4000000")
+board_runner_args(blackmagicprobe "--gdb-serial=/dev/ttyBmpGdb")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
